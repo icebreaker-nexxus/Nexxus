c7e1c79e2a032679409606d566e2add3e044bed0
Radhika Karandikar <radhikak@lab126.com>
2017-04-10 14:57:06 -0700

    Request location permission at runtime

--- app/src/main/java/com/icebreakers/nexxus/activities/EventListActivity.java
+++ app/src/main/java/com/icebreakers/nexxus/activities/EventListActivity.java
@@ -1,10 +1,14 @@
 package com.icebreakers.nexxus.activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -16,8 +20,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import butterknife.BindView;
-import butterknife.ButterKnife;
+
 import com.bumptech.glide.Glide;
 import com.icebreakers.nexxus.NexxusApplication;
 import com.icebreakers.nexxus.R;
@@ -29,15 +32,19 @@ import com.icebreakers.nexxus.models.Profile;
 import com.icebreakers.nexxus.utils.EndlessRecyclerViewScrollListener;
 import com.icebreakers.nexxus.utils.ItemClickSupport;
 import com.icebreakers.nexxus.utils.LocationProvider;
+
 import org.parceler.Parcels;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import retrofit2.Call;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 import rx.subscriptions.CompositeSubscription;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static com.icebreakers.nexxus.activities.ProfileActivity.PROFILE_EXTRA;
 
@@ -46,7 +53,12 @@ public class EventListActivity extends BaseActivity
 
     private static final String TAG = NexxusApplication.BASE_TAG + EventListActivity.class.getName();
 
-    private LocationProvider mLocationProvider;
+    /**
+     * Id to identify a location permission request.
+     */
+    private static final int REQUEST_LOCATION_PERMISSION = 0;
+
+    private LocationProvider mLocationProvider = null;
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
@@ -81,7 +93,7 @@ public class EventListActivity extends BaseActivity
         @Override
         public void onItemClicked(RecyclerView recyclerView, int position, View v) {
 
-           // launch Event details activity
+            // launch Event details activity
             MeetupEvent event = events.get(position);
 
             Intent detailsActivityIntent = new Intent(EventListActivity.this, EventDetailsActivity.class);
@@ -102,7 +114,7 @@ public class EventListActivity extends BaseActivity
 
         setSupportActionBar(toolbar);
 
-        mLocationProvider = new LocationProvider(this, this);
+//        mLocationProvider = new LocationProvider(this, this);
 
         compositeSubscription = new CompositeSubscription();
 
@@ -136,7 +148,7 @@ public class EventListActivity extends BaseActivity
             @Override
             public void onRefresh() {
                 // TODO search for more events
-                 fetchMeetupEvents(lastKnownLocation);
+                fetchMeetupEvents(lastKnownLocation);
             }
         });
 
@@ -152,13 +164,83 @@ public class EventListActivity extends BaseActivity
     @Override
     protected void onResume() {
         super.onResume();
-        mLocationProvider.connect();
+
+        if (mLocationProvider == null) {
+            // check permissions
+            Log.i(TAG, "Checking if the app has necessary LOCATION permissions");
+            // Verify that all required contact permissions have been granted.
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                // Location permissions have not been granted.
+                Log.i(TAG, "Location permissions has NOT been granted. Requesting permissions.");
+                requestLocationPermissions();
+
+            } else {
+
+                // location permissions have been granted. Connect location provider
+                Log.i(TAG, "Location permissions have already been granted. Creating LocationProvider instance.");
+                mLocationProvider = new LocationProvider(this, this);
+                mLocationProvider.connect();
+            }
+        }
+
+    }
+
+    private void requestLocationPermissions() {
+        // Should we show an explanation?
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+            // Show an explanation to the user *asynchronously* -- don't block
+            // this thread waiting for the user's response! After the user
+            // sees the explanation, try again to request the permission.
+                Log.i(TAG,
+                        "Displaying location permission rationale to provide additional context.");
+                Snackbar.make(swipeRefreshLayout, R.string.location_permission_rationale,
+                        Snackbar.LENGTH_INDEFINITE)
+                        .setAction(R.string.ok, new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                ActivityCompat.requestPermissions(EventListActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
+                                        REQUEST_LOCATION_PERMISSION);
+                            }
+                        })
+                        .show();
+
+        } else {
+            // No explanation needed, we can request the permission.
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_PERMISSION);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_LOCATION_PERMISSION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the location-related task you need to do.
+                    if (mLocationProvider == null) {
+                        mLocationProvider = new LocationProvider(this, this);
+                        mLocationProvider.connect();
+                    }
+
+                } else {
+                    // permission denied, boo! Disable the functionality that depends on this permission.
+                    // TODO how do we handle this?
+                }
+            }
+        }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        mLocationProvider.disconnect();
+        if (mLocationProvider != null)
+            mLocationProvider.disconnect();
     }
 
     @Override
