d357f2b0d80460cd8d5512f92e2ca457155a6d2a
Aditya Modi <amodi@linkedin.com>
2017-04-09 17:56:07 -0700

    UI Polish to Profiel Detail view

--- app/src/main/java/com/icebreakers/nexxus/fragments/ProfileFragment.java
+++ app/src/main/java/com/icebreakers/nexxus/fragments/ProfileFragment.java
@@ -3,21 +3,20 @@ package com.icebreakers.nexxus.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import com.bumptech.glide.Glide;
 import com.icebreakers.nexxus.NexxusApplication;
 import com.icebreakers.nexxus.R;
 import com.icebreakers.nexxus.models.Profile;
-
 import org.parceler.Parcels;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 import static com.icebreakers.nexxus.activities.ProfileActivity.PROFILE_EXTRA;
 
@@ -32,6 +31,7 @@ public class ProfileFragment extends Fragment {
     @BindView(R.id.tvName) TextView tvProfileName;
     @BindView(R.id.tvHeadline) TextView tvHeadline;
     @BindView(R.id.ivProfileImage) ImageView ivProfileImage;
+    @BindView(R.id.toolbar) Toolbar toolbar;
 
     private Profile profile;
 
@@ -56,7 +56,10 @@ public class ProfileFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
         ButterKnife.bind(this, view);
-
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayShowTitleEnabled(false);
         tvProfileName.setText(profile.firstName + " " + profile.lastName);
         tvHeadline.setText(profile.headline);
         Glide.with(getActivity()).load(profile.pictureUrl).into(ivProfileImage);
