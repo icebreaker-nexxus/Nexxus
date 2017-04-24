0bc450e3d17428b8b6ebf5a961aa3b1124f60da9
Aditya Modi <amodi@linkedin.com>
2017-04-23 22:20:55 -0700

    Adding code to handle incoming notification

--- app/src/main/java/com/icebreakers/nexxus/services/NexxusFirebaseInstanceIDService.java
+++ app/src/main/java/com/icebreakers/nexxus/services/NexxusFirebaseInstanceIDService.java
@@ -1,10 +1,38 @@
 package com.icebreakers.nexxus.services;
 
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.icebreakers.nexxus.persistence.NexxusSharePreferences;
 
 /**
  * Created by amodi on 4/23/17.
  */
 
 public class NexxusFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    private static final String TAG = NexxusFirebaseInstanceIDService.class.getSimpleName();
+
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        // Saving reg id to shared preferences
+        NexxusSharePreferences.putPushRegistrationId(getApplicationContext(), refreshedToken);
+
+        // sending reg id to your server
+        sendRegistrationToServer(refreshedToken);
+
+        // Notify UI that registration has completed, so the progress indicator can be hidden.
+        Intent registrationComplete = new Intent("registration");
+        registrationComplete.putExtra("token", refreshedToken);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(registrationComplete);
+    }
+
+    private void sendRegistrationToServer(final String token) {
+        // sending gcm token to server
+        Log.e(TAG, "sendRegistrationToServer: " + token);
+    }
 }
