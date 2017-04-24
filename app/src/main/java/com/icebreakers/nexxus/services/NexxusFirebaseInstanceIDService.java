9aa0da6fb2b1f2f6fe09a482237066333d525905
Aditya Modi <amodi@linkedin.com>
2017-04-23 21:25:37 -0700

    Notifications setup

---
+++ app/src/main/java/com/icebreakers/nexxus/services/NexxusFirebaseInstanceIDService.java
@@ -0,0 +1,10 @@
+package com.icebreakers.nexxus.services;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by amodi on 4/23/17.
+ */
+
+public class NexxusFirebaseInstanceIDService extends FirebaseInstanceIdService {
+}
