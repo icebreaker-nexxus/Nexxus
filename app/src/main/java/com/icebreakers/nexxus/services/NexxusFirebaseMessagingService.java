2d719d6abec88fe15f0a2752b2f5983f9ab1b882
Aditya Modi <amodi@linkedin.com>
2017-04-23 21:25:37 -0700

    Notifications setup

---
+++ app/src/main/java/com/icebreakers/nexxus/services/NexxusFirebaseMessagingService.java
@@ -0,0 +1,12 @@
+package com.icebreakers.nexxus.services;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Created by amodi on 4/23/17.
+ */
+
+public class NexxusFirebaseMessagingService extends FirebaseMessagingService {
+
+
+}
