828622ae0ae7009e9256e9ba9ca7ceed42bf7aff
Aditya Modi <amodi@linkedin.com>
2017-05-03 22:56:01 -0700

    Adding circular reveal animation to search

--- app/src/main/java/com/icebreakers/nexxus/activities/ProfileListActivity.java
+++ app/src/main/java/com/icebreakers/nexxus/activities/ProfileListActivity.java
@@ -1,8 +1,12 @@
 package com.icebreakers.nexxus.activities;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v4.util.Pair;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.LinearLayoutManager;
@@ -14,8 +18,11 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.EditText;
-
+import android.widget.ImageView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import com.icebreakers.nexxus.NexxusApplication;
 import com.icebreakers.nexxus.R;
 import com.icebreakers.nexxus.adapters.ProfileAdapter;
@@ -25,16 +32,13 @@ import com.icebreakers.nexxus.helpers.SimilaritiesFinder;
 import com.icebreakers.nexxus.models.Profile;
 import com.icebreakers.nexxus.models.Similarities;
 import com.icebreakers.nexxus.utils.ItemClickSupport;
+import jp.wasabeef.recyclerview.adapters.SlideInRightAnimationAdapter;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import jp.wasabeef.recyclerview.adapters.SlideInRightAnimationAdapter;
 
 /**
  * Created by amodi on 4/8/17.
@@ -44,10 +48,12 @@ public class ProfileListActivity extends BaseActivity {
 
     private static final String TAG = NexxusApplication.BASE_TAG + ProfileListActivity.class.getSimpleName();
     @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.searchtoolbar) Toolbar searchToolbar;
+    @BindView(R.id.rvProfiles) RecyclerView rvProfiles;
 
-    @BindView(R.id.rvProfiles)
-    RecyclerView rvProfiles;
-
+    MenuItem searchItem;
+    Menu searchMenu;
+    Menu menu;
     ProfileAdapter profileAdapter;
     RecyclerView.LayoutManager layoutManager;
     Profile currentProfile;
@@ -88,52 +94,13 @@ public class ProfileListActivity extends BaseActivity {
                 Router.startProfileActivity(ProfileListActivity.this, profile, p1);
             }
         });
+        setupSearchBar();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_profiles, menu);
-        MenuItem searchItem = menu.findItem(R.id.action_search);
-        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
-
-        int searchEditId = android.support.v7.appcompat.R.id.search_src_text;
-        EditText et = (EditText) searchView.findViewById(searchEditId);
-        et.setTextColor(Color.WHITE);
-        et.setHintTextColor(Color.WHITE);
-        et.setHint(R.string.search_by);
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                performSearch(query);
-                return true;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                if (profileAdapter.getItemCount() != allAttendees.size() && newText.isEmpty()) {
-                    Log.d(TAG, "Search query is empty, calling updateProfileList");
-                    updateProfileList(allAttendees);
-                }
-                return true;
-            }
-        });
-
-
-        MenuItemCompat.setOnActionExpandListener(menu.findItem(R.id.action_search), new MenuItemCompat.OnActionExpandListener() {
-            @Override
-            public boolean onMenuItemActionExpand(MenuItem item) {
-                return true;
-            }
-
-            @Override
-            public boolean onMenuItemActionCollapse(MenuItem item) {
-                // SearchView is closing
-                updateProfileList(allAttendees);
-                return true;
-            }
-        });
 
         return super.onCreateOptionsMenu(menu);
 
@@ -198,8 +165,14 @@ public class ProfileListActivity extends BaseActivity {
         switch (id) {
             case R.id.action_search:
                 final SearchView searchView = (SearchView) MenuItemCompat.getActionView(item);
-                item.expandActionView();
-                searchView.requestFocus();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    circleReveal(R.id.searchtoolbar, true);
+                }
+                else {
+                    searchToolbar.setVisibility(View.VISIBLE);
+                }
+
+                searchItem.expandActionView();
                 return true;
 
             case android.R.id.home:
@@ -210,4 +183,124 @@ public class ProfileListActivity extends BaseActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void setupSearchBar() {
+        searchToolbar.inflateMenu(R.menu.menu_search);
+        searchMenu = searchToolbar.getMenu();
+
+        searchToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+                    circleReveal(R.id.searchtoolbar, false);
+                else
+                    searchToolbar.setVisibility(View.GONE);
+            }
+        });
+
+        searchItem = searchMenu.findItem(R.id.action_filter_search);
+
+        MenuItemCompat.setOnActionExpandListener(searchItem, new MenuItemCompat.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                // Do something when collapsed
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    circleReveal(R.id.searchtoolbar, false);
+                }
+                else {
+                    searchToolbar.setVisibility(View.GONE);
+                }
+                updateProfileList(allAttendees);
+                return true;
+            }
+
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                // Do something when expanded
+                return true;
+            }
+        });
+
+        initSearchView();
+    }
+
+    public void initSearchView() {
+        final SearchView searchView = (SearchView) searchMenu.findItem(R.id.action_filter_search).getActionView();
+
+        // Enable/Disable Submit button in the keyboard
+
+        searchView.setSubmitButtonEnabled(false);
+
+        // Change search close button image
+
+        ImageView closeButton = (ImageView) searchView.findViewById(R.id.search_close_btn);
+        closeButton.setImageResource(R.drawable.ic_close);
+
+
+        // set hint and the text colors
+
+        int searchEditId = android.support.v7.appcompat.R.id.search_src_text;
+        EditText et = (EditText) searchView.findViewById(searchEditId);
+        et.setTextColor(Color.BLACK);
+        et.setHintTextColor(Color.BLACK);
+        et.setHint(R.string.search_by);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                performSearch(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (profileAdapter.getItemCount() != allAttendees.size() && newText.isEmpty()) {
+                    Log.d(TAG, "Search query is empty, calling updateProfileList");
+                    updateProfileList(allAttendees);
+                }
+                return true;
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void circleReveal(int viewID, final boolean shouldMakeVisible) {
+        final View myView = findViewById(viewID);
+
+        int width = toolbar.getWidth();
+
+        width -= (getResources().getDimensionPixelSize(R.dimen.abc_action_button_min_width_material)) -
+            (getResources().getDimensionPixelSize(R.dimen.abc_action_button_min_width_material) / 2) -
+            getResources().getDimensionPixelSize(R.dimen.abc_action_button_min_width_overflow_material);
+
+        int cx = width;
+        int cy = toolbar.getHeight()/2;
+
+        Animator anim;
+        if(shouldMakeVisible) {
+            anim = ViewAnimationUtils.createCircularReveal(myView, cx, cy, 0, (float) width);
+        }
+        else {
+            anim = ViewAnimationUtils.createCircularReveal(myView, cx, cy, (float) width, 0);
+        }
+
+        anim.setDuration(1000);
+
+        // make the view invisible when the animation is done
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                if(!shouldMakeVisible) {
+                    super.onAnimationEnd(animation);
+                    myView.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        // make the view visible and start the animation
+        if (shouldMakeVisible) {
+            myView.setVisibility(View.VISIBLE);
+        }
+        // start the animation
+        anim.start();
+    }
 }
